de6a49d2c025c4d288592849d93c9298572784e8
David Gardiner <david@gardiner.net.au>
2025-07-31 19:13:48 +0930

    Add Cosmos

--- src/AspireCosmosPythonWeb.AppHost/AppHost.cs
+++ src/AspireCosmosPythonWeb.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cosmos = builder.AddAzureCosmosDB("cosmos-db")
+    .RunAsEmulator(emulator =>
+    {
+        emulator.WithDataVolume();
+    });
+
 builder.Build().Run();
